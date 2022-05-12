7eee4d99557a4ae76a6a025a42690bea4db31706
amin <aminqanbarii1992@gmail.com>
2022-05-12 11:41:46 -0700

    add Box to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import StoriesContainer from "./containers/StoriesContainer";
 
+import { Box } from "@mui/system";
+
 function App() {
   return (
-    <div className="App">
-      <StoriesContainer />
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "background.default",
+          color: "text.primary",
+          p: 3,
+        }}
+      >
+        <StoriesContainer />
+      </Box>
     </div>
   );
 }
