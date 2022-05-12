10a8fd3cc831ee2ba2e764edf9aadea7ce14c195
amin <aminqanbarii1992@gmail.com>
2022-05-12 11:40:58 -0700

    edit api

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -7,11 +7,11 @@ export const storyUrl = `${baseUrl}item/`;
 export const getStory = async (storyId) => {
   const result = await axios
     .get(`${storyUrl + storyId}.json`)
-    .then(({ data }) => data);
+    .then((response) => response.data);
   return result;
 };
 
 export const getStoryIds = async () => {
-  const result = await axios.get(newStoriesUrl).then(({ data }) => data);
+  const result = await axios.get(newStoriesUrl).then((response) => response.data);
   return result;
 };
