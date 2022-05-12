e6476fb6451d044567cb7575dc1bc37b840ac095
amin <aminqanbarii1992@gmail.com>
2022-05-12 11:40:01 -0700

    styled with material ui

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,20 +2,86 @@ import React, { useEffect, useState } from "react";
 import { mapTime } from "../helper/mapTime";
 import { getStory } from "../services/newsApi";
 
-const Story = ({ storyId }) => {
+//material-ui
+import { Card, CardContent, Link, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    margin: theme.spacing(2, 0),
+    padding: theme.spacing(1, 2, 0, 2),
+    borderRadius: "10px !important",
+  },
+  a: {
+    textDecoration: "none !important",
+
+    "&:hover": {
+      color: "purple !important",
+    },
+
+    [theme.breakpoints.down("md")]: {
+      fontSize: "16px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "13px",
+    },
+  },
+
+  cardContent: {
+    display: "flex",
+    alignItems: "center",
+    paddingBottom: theme.spacing(0),
+    color: theme.palette.text.disabled,
+
+    [theme.breakpoints.down("md")]: {
+      fontSize: "1px",
+    },
+    "&:last-child": {
+      padding: 0,
+    },
+  },
+
+  subtitle1: {
+    [theme.breakpoints.down("md")]: {
+      fontSize: "14px !important",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "10px !important",
+    },
+  },
+}));
+
+const Story = ({ newsId }) => {
   const [story, setstory] = useState({});
+
+  const classes = useStyles();
+
   useEffect(() => {
-    getStory(storyId).then((data) => data && data.url && setstory(data));
-  }, [storyId]);
-  return story && story.url ? (
-    <>
-      <a href={story.url}>
-        {story.title}
-      </a>
-      By: <p>{story.by}</p>
-      Posted: <p>{mapTime(story.time)}</p>
-    </>
-  ) : null;
+    getStory(newsId).then((data) => data && data.url && setstory(data));
+  }, [newsId]);
+console.log(story);
+  return (
+    <div>
+      {story && story.url ? (
+        <Card className={classes.card} sx={{}}>
+          <Typography variant="h6">
+            <Link className={classes.a} href={story.url}>
+              {story.title}
+            </Link>
+          </Typography>
+          <CardContent className={classes.cardContent}>
+            <Typography className={classes.subtitle1}> by {story.by}</Typography>
+            <Typography className={classes.subtitle1} variant="subtitle1">
+              |
+            </Typography>
+            <Typography className={classes.subtitle1}>
+              posted: {mapTime(story.time)}
+            </Typography>
+          </CardContent>
+        </Card>
+      ) : null}
+    </div>
+  );
 };
 
 export default Story;
